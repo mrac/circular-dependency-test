9361932dc4a44cf262702dba05eb3c4c6bf73011
Michal Raczkowski <michal.raczkowski@zalando.de>
2018-04-21 22:44:59 +0200

    output fn

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
 #!/usr/bin/env node
 
-const webpack = require('webpack');
-const CircularDependencyPlugin = require('circular-dependency-plugin');
+const webpack = require("webpack");
+const CircularDependencyPlugin = require("circular-dependency-plugin");
 
 const args = parseArgs(process.argv);
-const webpackConfigPath = args.config || 'webpack.config.js';
-const excludeRegExp = new RegExp(args.exclude || 'node_modules');
+const webpackConfigPath = args.config || "webpack.config.js";
+const excludeRegExp = new RegExp(args.exclude || "node_modules");
 
 const cwd = process.cwd();
-const webpackConfig = require(cwd + '/' + webpackConfigPath);
+const webpackConfig = require(cwd + "/" + webpackConfigPath);
 
 runWebpack(webpackConfig);
 
 function runWebpack(webpackConfig) {
-  webpack(config(webpackConfig)).run((err, stats) => {
-    if (err || stats.hasErrors()) {
-      const fileMap = {};
-      const errors = stats.toJson().errors;
-      let fileCount;
-
-      errors.forEach(error => {
-        const files = error.replace('Circular dependency detected:', '').split(' -> ');
-        files.forEach(file => {
-          fileMap[file] = (fileMap[file] || 0) + 1;
-        });
-        console.log('');
-        console.log('\x1b[31m%s\x1b[0m', error);
-      });
-
-      fileCount = Object.keys(fileMap).length;
-
-      console.log('');
-      console.log('\x1b[31m%s\x1b[0m', `DETECTED ${errors.length} CIRCULAR-DEPENDENCY ISSUES.`);
-      console.log('\x1b[31m%s\x1b[0m', `INVOLVED ${fileCount} FILES.`);
-      console.log('');
-      process.exit(1);
-    } else {
-      console.log('');
-      console.log('\x1b[32m%s\x1b[0m', 'No circular dependencies detected.');
-      console.log('');
-    }
-  });
+  webpack(config(webpackConfig)).run(output);
 }
 
 function config(webpackConfig) {
@@ -56,6 +29,40 @@ function config(webpackConfig) {
       })
     ]
   };
+}
+
+function output(err, stats) {
+  if (err || stats.hasErrors()) {
+    const fileMap = {};
+    const errors = stats.toJson().errors;
+    let fileCount;
+
+    errors.forEach(error => {
+      const files = error
+        .replace("Circular dependency detected:", "")
+        .split(" -> ");
+      files.forEach(file => {
+        fileMap[file] = (fileMap[file] || 0) + 1;
+      });
+      console.log("");
+      console.log("\x1b[31m%s\x1b[0m", error);
+    });
+
+    fileCount = Object.keys(fileMap).length;
+
+    console.log("");
+    console.log(
+      "\x1b[31m%s\x1b[0m",
+      `DETECTED ${errors.length} CIRCULAR-DEPENDENCY ISSUES.`
+    );
+    console.log("\x1b[31m%s\x1b[0m", `INVOLVED ${fileCount} FILES.`);
+    console.log("");
+    process.exit(1);
+  } else {
+    console.log("");
+    console.log("\x1b[32m%s\x1b[0m", "No circular dependencies detected.");
+    console.log("");
+  }
 }
 
 function parseArgs(argv) {
